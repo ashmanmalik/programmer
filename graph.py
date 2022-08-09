3d3017d7aaf581378d990e1a2edce9cc574ad263
Ash <ash@Admins-MacBook-Pro-2.local>
2022-08-09 14:45:46 +1000

    commit

--- graph.py
+++ graph.py
@@ -1,10 +1,13 @@
-import os , random
+import os 
+from random import randint
 
-for i in range(100):
-    d = str(i) + 'days ago'
-    rand = random.randrange(6, 12)
-    with open('test.txt','a') as file:
-        file.write(d+'\n')
-    os.system('git add test.txt')
-    os.system('git commit --date=" 2021-'+str(rand)+'-'+d+'" -m 1')
+for i in range(1, 365):
+
+    for j in range (0, randint(1, 10)): 
+        d = str(i) + 'days ago'
+        with open('test.txt','a') as file:
+            file.write(d+'\n')
+        os.system('git add .')
+        os.system('git commit --date="'+ d +'" -m "commit"')
+
 os.system('git push -u origin main')
